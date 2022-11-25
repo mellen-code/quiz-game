f6b48f60cbed0e4752c92948b889c2fc0696b7af
MSmith <marie.ellen@gmail.com>
2022-11-25 15:27:55 -0500

    initial files

---
+++ js/main.js
@@ -0,0 +1,47 @@
+let playerScore = document.querySelector('#playerScore');
+
+let score = 0;
+
+playerScore.innerHTML = score;
+
+document.querySelector('.styling').addEventListener('click', styling)
+
+document.querySelector('.language').addEventListener('click', language)
+
+document.querySelector('.method').addEventListener('click', method)
+
+function styling() {
+    let stylingAnswer = document.querySelector('#stylingAnswer').value
+
+    if (stylingAnswer.toLowerCase() == 'css') {
+        score++;
+        playerScore.innerHTML = score;
+        document.querySelector('#stylingRightOrWrong').innerHTML = 'Correct!';
+    }else{
+        document.querySelector('#stylingRightOrWrong').innerHTML = 'Nope, the answer is: CSS'
+    }
+}
+
+function language() {
+    let languageAnswer = document.querySelector('#languageAnswer').value
+
+    if (languageAnswer.toLowerCase() == 'markup') {
+        score++;
+        playerScore.innerHTML = score;
+        document.querySelector('#languageRightOrWrong').innerHTML = 'Correct!';
+    }else{
+        document.querySelector('#languageRightOrWrong').innerHTML = 'Nope, the answer is: markup'
+    }
+}
+
+function method() {
+    let methodAnswer = document.querySelector('#methodAnswer').value
+
+    if (methodAnswer.toLowerCase() == 'behavior') {
+        score++;
+        playerScore.innerHTML = score;
+        document.querySelector('#methodRightOrWrong').innerHTML = 'Correct!';
+    }else{
+        document.querySelector('#methodRightOrWrong').innerHTML = 'Nope, the answer is: behavior'
+    }
+}
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+app.use(cors())
+
+const PORT = 8000
+
+const answerBank = {
+    'styling': {
+        'html': 'Correct!',
+        'wrong': 'Nope, answer is HTML'
+    },
+    'wrong': {
+        
+    }
+}
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/question1', (request, response) => {
+    response.sendFile(__dirname + '/question1.html')
+    let userAnswer = request.params.answer
+    if(answerBank[userAnswer]) {
+        response.json(answerBank[userAnswer])
+    }else{
+        response.json(answerBank[userAnswer.wrong])
+    }
+})
+
+// environment variable for host to use own port OR use our defined port of 8000:
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`The server is now running on port ${PORT}!`)
+})
